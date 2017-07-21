d407890a2e0a317d111461ea8b53254639273460
RENT <damiasz@op.pl>
2017-07-21 21:03:15 +0200

    Polimorfizm i przeciążanie metod

---
+++ src/trzeciTydzien/piatek/Main.java
@@ -0,0 +1,13 @@
+package trzeciTydzien.piatek;
+
+/**
+ * Created by RENT on 2017-07-21.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Punkt3D punkt3D = new Punkt3D(13, 12, 17);
+
+        System.out.println(punkt3D.opiszPunkty());
+    }
+}
