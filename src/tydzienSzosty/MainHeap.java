84f13c41c017e180dd76b2196aee8c9a3f64c1e3
olaIdamian <damiasz@op.pl>
2017-08-07 23:02:36 +0200

    Array list continue

---
+++ src/tydzienSzosty/MainHeap.java
@@ -0,0 +1,30 @@
+package tydzienSzosty;
+
+/**
+ * Created by olaIdamian on 8/7/2017.
+ */
+public class MainHeap {
+    public static void main(String[] args) {
+        ListMergeSort list = new ListMergeSort();
+
+        list.add(1);
+        list.add(3);
+        list.add(6);
+
+        list.print();
+
+        System.out.println("------");
+        ListMergeSort list2 = new ListMergeSort();
+
+        list2.add(2);
+        list2.add(5);
+        list2.add(6);
+
+        list2.print();
+        System.out.println("------");
+        list.mergeLists(list2);
+        list.print();
+        System.out.println("------");
+        list2.print();
+    }
+}
