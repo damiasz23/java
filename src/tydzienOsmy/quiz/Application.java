b5ccd4e583f9459af518a33eb6ad6debf9a5b3a2
olaIdamian <damiasz@op.pl>
2017-08-23 22:44:40 +0200

    Quiz game

--- src/tydzienOsmy/quiz/Application.java
+++ src/tydzienOsmy/quiz/Application.java
@@ -1,5 +1,10 @@
 package tydzienOsmy.quiz;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,43 +15,79 @@ public class Application {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.println("---------Witaj w programie Quiz--------\n");
-        int option = 0;
+        int option = -1;
+        while (option != 0) {
+            System.out.println("---------Witaj w programie Quiz--------\n");
 
-        System.out.println("Co chcesz zrobić?");
-        System.out.println("1. Nowy Quiz");
-        System.out.println("2. Lista wyników");
-        System.out.println("0. Wyjscie z programu");
 
-        option = in.nextInt();
+            System.out.println("Co chcesz zrobić?");
+            System.out.println("1. Nowy Quiz");
+            System.out.println("2. Lista wyników");
+            System.out.println("0. Wyjscie z programu");
 
-        switch (option){
-            case 1 :  {
-                System.out.println("Nowy Quiz");
-                Quiz quiz = new Quiz();
-                List<Question> questions = quiz.readAllQuestions();
-                for(Question q : questions){
-                    System.out.println(q);
-                }
-                break;
-            }
-            case 2: {
-                System.out.println("Lista wyników");
-                List<String> ranking = Quiz.getRanking();
-                if(ranking!=null){
-                    for (String wynik : ranking){
-                        System.out.println(wynik);
+            option = in.nextInt();
+
+            switch (option) {
+                case 1: {
+                    System.out.println("Pytania: ");
+                    Quiz quiz = new Quiz();
+                    List<Question> questions = quiz.readAllQuestions();
+
+
+                    System.out.println("Podaj swój nick");
+                    String nick = in.next();
+
+                    int correctAnswersCounter = 0;
+                    for (int i =0  ;i <questions.size() ;i++) {
+                        System.out.println("Pytanie nr: " + (i +1));
+                        Question que = questions.get(i);
+                        System.out.println(que);
+
+
+                        System.out.println("Podaj odpowiedz");
+                        String answer = in.next();
+
+                        if(que.getCorrectAnswers().equals(answer)){
+                            System.out.println("-----Dobrze!!!-----");
+                            correctAnswersCounter++;
+                        }
+                        else {
+                            System.out.println("-----Żle-----");
+                        }
                     }
-                }
-                else{
-                    System.out.println("Brak wyników");
-                }
-                break;
-            }
-            case 0: {
-                System.out.println("Wyjście");
-            }
-        }
+                    System.out.println();
+                    System.out.println("Twój wynik to: " + correctAnswersCounter + "/" + questions.size() + "\n");
 
+                    String score = "Nick: " + nick + "  " + "Ilość punktów: " + correctAnswersCounter + "\n";
+
+                    Path pathToResult = Paths.get("./src/tydzienOsmy/quiz/wyniki.txt");
+                    try {
+                        Files.write(pathToResult, score.getBytes(), Files.exists(pathToResult) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                case 2: {
+                    System.out.println("Lista wyników");
+                    List<String> ranking = Quiz.getRanking();
+                    if (ranking != null) {
+                        for (String wynik : ranking) {
+                            System.out.println(wynik);
+                        }
+                    } else {
+                        System.out.println("Brak wyników");
+                    }
+                    break;
+                }
+                case 3: {
+                    System.out.println("Twoja odpowiedz to: ");
+                }
+                case 0: {
+                    System.out.println("Wyjście");
+                }
+            }
+
+        }
     }
 }
--- src/tydzienOsmy/quiz/Quiz.java
+++ src/tydzienOsmy/quiz/Quiz.java
@@ -6,35 +6,36 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Created by olaIdamian on 8/23/2017.
  */
 public class Quiz {
-    private static final String PATH_TO_RESULT = "./src/quiz/wyniki.txt";
-    public static final String PATH_TO_QUESTIONS = "./src/quiz/pytania.txt";
+    private static final String PATH_TO_RESULT = "./src/tydzienOsmy/quiz/wyniki.txt";
+    public static final String PATH_TO_QUESTIONS = "./src/tydzienOsmy/quiz/pytania.txt";
 
-    public static List<String> getRanking(){
+    public static List<String> getRanking() {
 
         List<String> result = null;
         try {
-            result = Files.readAllLines(Paths.get(PATH_TO_RESULT) );
+            result = Files.readAllLines(Paths.get(PATH_TO_RESULT));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    public static List<Question> readAllQuestions(){
+    public static List<Question> readAllQuestions() {
         List<String> questionsFromFile = null;
         List<Question> questionList = new ArrayList<>();
 
         try {
-            questionsFromFile = Files.readAllLines(Paths.get(PATH_TO_QUESTIONS) );
+            questionsFromFile = Files.readAllLines(Paths.get(PATH_TO_QUESTIONS));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for(int i = 0 ;i < questionsFromFile.size() ;i+=6){
+        for (int i = 0; i < questionsFromFile.size(); i += 6) {
             Question question = new Question();
             question.setTitle(questionsFromFile.get(i));
 
@@ -44,8 +45,11 @@ public class Quiz {
             question.getAnswers().add(questionsFromFile.get(i + 4));
             question.setCorrectAnswers(questionsFromFile.get(i + 5));
             questionList.add(question);
+
+
         }
         return questionList;
     }
-
 }
+
+
