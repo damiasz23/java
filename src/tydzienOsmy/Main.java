ca4509b76e0a79f98204c6c603cf879347b8bb57
olaIdamian <damiasz@op.pl>
2017-08-23 12:40:41 +0200

    File operations

---
+++ src/tydzienOsmy/Main.java
@@ -0,0 +1,12 @@
+package tydzienOsmy;
+
+import java.io.IOException;
+
+/**
+ * Created by olaIdamian on 8/21/2017.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileOverview.createDirectoryAndFIle();
+    }
+}
