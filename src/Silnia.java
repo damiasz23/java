bd7113d5bc3c3c30bddb1f999c24d23be5d16dca
olaIdamian <damiasz@op.pl>
2017-08-28 13:23:01 +0200

    Silnia iteracyjnie

--- src/Silnia.java
+++ src/Silnia.java
@@ -4,26 +4,15 @@
 import java.util.Scanner;
 public class Silnia {
     public static void main(String[] args){
-
-
             int silnia;
             Scanner in = new Scanner(System.in);
             System.out.println("Wproawdz liczbę naturalną: ");
+
             int n = in.nextInt();
-
-
             int iloczyn = 1;
             for (int i=1; i<=n; i++) {
                 iloczyn *= i;
-
-
-
             }
             System.out.println("Silnia liczby: " + n + " to " + iloczyn);
         }
-
     }
-
-
-
-
