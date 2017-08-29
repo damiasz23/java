3cf584544565ee54d6a38549a3da9b510cf83673
RENT <damiasz@op.pl>
2017-08-29 20:53:27 +0200

    Test

---
+++ src/src/main/java/excercise/Greeting.java
@@ -0,0 +1,31 @@
+package excercise;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Greeting {
+
+    public String greet(String... names) {
+
+        if (names == null) {
+            return "Hello, my friend";
+        }
+        if (names.length == 1) {
+            String name = names[0];
+
+            if (name.toUpperCase().equals(name)) {
+                return "HELLO " + name;
+            }
+
+            return "Hello, " + name;
+        }
+        String result = "Hello";
+        for(int i = 0;i < names.length;i++){
+            if(i == names.length-1){
+                result += " and " + names[i];
+            }else {
+                result += ", " + names[i];
+            }
+        }
+       return result;
+    }
+}
---
+++ src/src/test/java/excercise/GreetingTest.java
@@ -0,0 +1,60 @@
+package excercise;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreetingTest {
+    private Greeting greeting;
+
+
+    @Before
+    public void initialize(){
+        greeting = new Greeting();
+    }
+    @Test
+    public void greet()  {
+//        Greeting greeting = new Greeting();
+
+        String result = greeting.greet("Jan");
+
+        assertEquals("Hello, Jan", result);
+
+    }
+
+    @Test
+    public void greetWhenNull()  {
+//        Greeting greeting = new Greeting();
+
+        String result = greeting.greet(null);
+
+        assertEquals("Hello, my friend", result);
+
+    }
+
+    @Test
+    public void whenGreatSigns()  {
+
+//        Greeting greeting = new Greeting();
+
+        String result = greeting.greet("JAN");
+
+        assertEquals("HELLO JAN" , result);
+    }
+
+    @Test
+    public void tabName()  {
+
+        String result = greeting.greet("Jan", "Ala");
+
+        assertEquals("Hello, Jan and Ala", result);
+
+    }
+
+    @Test
+    public void multiplyNames() {
+        String result = greeting.greet("Piotr", "Anna", "Paulina");
+        assertEquals("Hello, Piotr, Anna and Paulina", result);
+    }
+}
